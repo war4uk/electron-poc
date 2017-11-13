639d07f5b781fda950ae2e80279d1e27f6914620
Alexander Polyakov <apolyakov@intermedia.net>
2017-11-13 12:07:40 +0300

    some fixes for better usability

--- my-new-project/src/project-one/src/app/app.component.ts
+++ my-new-project/src/project-one/src/app/app.component.ts
@@ -11,7 +11,6 @@ import pluginConfiguration from '../../plugin.config.json';
 export class AppComponent {
   title = 'app';
   constructor() {
-    console.log('1', pluginConfiguration.pluginSpecificChannelId);
     let internalBroadcastChannel = new (<any>window).BroadcastChannel(pluginConfiguration.pluginSpecificChannelId);
     internalBroadcastChannel.postMessage('Hello from project one');
   }
