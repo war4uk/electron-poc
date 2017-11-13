20d4d2d7c2563c7e89a53ad6144b3818ae04af57
Alexander Polyakov <apolyakov@intermedia.net>
2017-11-13 12:21:21 +0300

    remove obsolete message bus folder and fox port for http-server

--- my-new-project/src/message-bus/message-bus.js
+++
@@ -1,9 +0,0 @@
-const openedWindows = Object.create(null);
-
-exports.registerInMessageBus = function registerInMessageBus(id, windowHandle) {
-  openedWindows[id] = windowHandle;
-};
-
-exports.initializeMessaging = function initializeMessaging(id) {
-  openedWindows[id].webContents.executeJavaScript('alert("here we are!")');
-};
